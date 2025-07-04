2f535cb0f88ddedbd6ac68eb9b34c5b764cb3efe
k-tsunoda615 <kntrtnd@gmail.com>
2025-07-04 21:42:55 +0900

    tailwindの初期設定

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -4,13 +4,22 @@ function TopPage() {
   return (
     <div>
       <h1>TopPage</h1>
-      <div className="flex gap-4">
-        <Link to="/signup">サインアップ</Link>
-        <Link to="/login">ログイン</Link>
-        <Link to="/user/edit">ユーザー編集</Link>
-        <Link to="/list">リスト</Link>
-        <Link to="/post">書籍レビュー投稿</Link>
-        <Link to="/detail">書籍レビュー詳細</Link>
+      <div className="flex gap-4 justify-center mt-10 max-w-[900px] mx-auto">
+        <Link to="/signup" className="p-1 border border-gray-300 rounded-md">
+          サインアップ
+        </Link>
+        <Link to="/login" className="p-1 border border-gray-300 rounded-md">
+          ログイン
+        </Link>
+        <Link to="/user/edit" className="p-1 border border-gray-300 rounded-md">
+          ユーザー編集
+        </Link>
+        <Link to="/list" className="p-1 border border-gray-300 rounded-md">
+          リスト
+        </Link>
+        <Link to="/post" className="p-1 border border-gray-300 rounded-md">
+          書籍レビュー投稿
+        </Link>
       </div>
     </div>
   );
